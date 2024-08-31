37ba630158200dc6a74a3049c823e8a97d31d70a
Yassir Rouane <yasserro100@gmail.com>
2024-08-31 01:50:50 +0100

    feat(ui): Implement loading state for signup, signin, and Google auth buttons

--- client/src/components/Buttons/Btn.tsx
+++ client/src/components/Buttons/Btn.tsx
@@ -3,11 +3,12 @@ interface ButtonProps {
   id?: string
   children: React.ReactNode | string
   onClick?: () => void
+  disabled?: boolean
 }
 
-const Button = ({ type, id, children, onClick }: ButtonProps) => {
+const Button = ({ type, id, children, onClick, disabled }: ButtonProps) => {
   return (
-    <button id={id} type={type} onClick={onClick}>
+    <button id={id} type={type} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   )
