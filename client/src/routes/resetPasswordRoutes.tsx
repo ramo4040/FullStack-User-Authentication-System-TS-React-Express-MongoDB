9ca16e0472891e4200c2a000f32c35a7f3be0e8d
Yassir Rouane <yasserro100@gmail.com>
2024-08-31 01:52:22 +0100

    feat(ui) : show loading indicator while user enter protected page (reset password)

--- client/src/routes/resetPasswordRoutes.tsx
+++ client/src/routes/resetPasswordRoutes.tsx
@@ -2,14 +2,18 @@ import { Navigate, useSearchParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import { ValidateResetToken } from '../services/authService'
+import useLoader from '../hooks/useLoader'
 
 function ResetPasswordRoutes({ children }: { children: React.ReactNode }) {
   const { showNotification } = useAuth()
   const [searchParams] = useSearchParams()
-  const [isValid, setIsValid] = useState<boolean>(true)
+  const [isValid, setIsValid] = useState<boolean | null>(null)
+  const { showLoading, hideLoading, LoaderElement } = useLoader()
 
   useEffect(() => {
     const validateToken = async () => {
+      showLoading()
+
       const { success, message } = await ValidateResetToken(
         searchParams.get('token'),
       )
@@ -19,21 +23,26 @@ function ResetPasswordRoutes({ children }: { children: React.ReactNode }) {
           message: message as string,
           type: 'error',
         })
+        hideLoading()
         setIsValid(false)
-        return
+      } else {
+        hideLoading()
+        setIsValid(true)
       }
-
-      setIsValid(true)
     }
-    validateToken()
-  }, [])
 
-  // Redirect based on validation result
+    validateToken()
+  }, [searchParams, showNotification])
+
+  if (isValid === null) {
+    return LoaderElement({})
+  }
+
   if (!isValid) {
     return <Navigate to="/login" replace />
   }
 
-  return <>{children}</> // Render children if token is valid
+  return <>{children}</>
 }
 
 export default ResetPasswordRoutes
